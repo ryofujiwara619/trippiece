86eb24cd6e6c32df4355256d0f344876e916d69e
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-22 20:26:28 +0900

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.8.2'
 
 set :application, 'trippiece'
-set :repo_url,  'git@github.com:<ryofujiwara619>/<trippiece>.git'
+set :repo_url,  'git@github.com:ryofujiwara619/trippiece.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
