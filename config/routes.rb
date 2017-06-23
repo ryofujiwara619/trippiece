0701506a35f29ccb2de940edd2f21d63f9675107
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-23 11:52:48 +0900

    Create rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "projects#index"
+  resources :users, only:[:show, :edit, :update]
+  resources :projects do
+    resources :comments, only:[:index, :create]
+  end
 end
