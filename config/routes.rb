549fe45b3bc3bec10ce6e49958f60dfa4fce9e4a
Ryo Fujiwara <frmtricolore@gmail.com>
2017-06-24 16:51:13 +0900

    Create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "projects#index"
   resources :users, only:[:show, :edit, :update]
-  devise_for :users
   resources :projects do
     resources :comments, only:[:index, :create]
   end
