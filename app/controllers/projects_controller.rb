cfe484a6c7cf1c4180ca2c21af423a431ac1938d
Ryo Fujiwara <frmtricolore@gmail.com>
2017-07-01 13:47:36 +0900

    Create project#create controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,22 @@
 class ProjectsController < ApplicationController
   def index
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      redirect_to new_project_path
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :image, :introduction, :destination, :departure_date, :finish_date)
+  end
 end
