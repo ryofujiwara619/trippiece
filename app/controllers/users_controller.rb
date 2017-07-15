e9e5627adb339d0ff6cbfa4167b57ec42513a245
Ryo Fujiwara <frmtricolore@gmail.com>
2017-07-15 17:09:39 +0900

    Create the view to show user's review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@ before_action :userProject
   def show
     @user = User.find(params[:id])
     @projects = @user.projects
-    @reviews = Review.where(user_id: params[:id])
-    @rate = @reviews.average(:rate).round(1)
+    if @user.reviews.present?
+      @reviews = @user.reviews
+      @rate = @reviews.average(:rate).round(1)
+    end
   end
 
   def update
