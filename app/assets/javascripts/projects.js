2d45582fad44030bbfeaf7971a03a80445f22327
Ryo Fujiwara <frmtricolore@gmail.com>
2017-07-27 17:46:06 +0900

    Create slide event

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,11 +1,19 @@
 $(document).on('turbolinks:load', function(){
-  $(".top-wrapper__middle--slide--changeBtns--changeBtn").click(function(){
-    var $displaySlide = $(".active");
-    $displaySlide.removeClass("active");
-    if($(this).hasClass("next")){
-      $displaySlide.next().addClass("active");
-    }else{
-      $displaySlide.prev().addClass("active");
-    }
+  // $(".top-wrapper__middle--slide--changeBtns--changeBtn").click(function(){
+  //   console.log("ああ")
+  //   var $displaySlide = $(".active");
+  //   $displaySlide.removeClass("active");
+  //   if($(this).hasClass("next")){
+  //     $displaySlide.next().addClass("active");
+  //   }else{
+  //     $displaySlide.prev().addClass("active");
+  //   }
+  // });
+  $('.slider').slick({
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    fade: true,
   });
 });
