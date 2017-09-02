609aeebe758a8372219257d73389b0fff32450dd
Ryo Fujiwara <frmtricolore@gmail.com>
2017-09-02 17:53:05 +0900

    Fix user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable
 
   mount_uploader :image, ImageUploader
   has_many :projects, through: :planners
@@ -12,10 +12,13 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.avatar = auth.info.image
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
     end
   end
 
