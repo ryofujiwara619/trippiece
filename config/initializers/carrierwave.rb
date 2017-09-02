542cb7b39d2190a22cff964a54e1444c641758c9
Ryo Fujiwara <frmtricolore@gmail.com>
2017-09-02 11:28:30 +0900

    Fix carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,6 +11,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ryo-fujiwara'
+  config.fog_directory  = 'upload-test-ryo'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ryo-fujiwara'
 end
