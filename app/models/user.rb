d21910b2f3b9a285d3eba124c3ec5f987a7c7f85
Ryo Fujiwara <frmtricolore@gmail.com>
2017-09-03 22:26:26 +0900

    Create find_for_google_oauth2 method

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,18 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.find_for_google_oauth2(auth)
+    user = User.where(email: auth.info.email).first
+
+    unless user
+      user = User.create(name:     auth.info.name,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         email:    auth.info.email,
+                         token:    auth.credentials.token,
+                         password: Devise.friendly_token[0, 20])
+    end
+    user
+  end
+
 end
